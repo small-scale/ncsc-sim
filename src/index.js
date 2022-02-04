b60337dd90690d69dcc722f828c00584ee610bbb
Keith Porcaro <keith@porca.ro>
2022-02-04 11:30:17 -0500

    fix scroll issue

--- src/index.js
+++ src/index.js
@@ -11,38 +11,45 @@ import RankingTwo from "./view/pages/ranking2"
 const AppRouter = ()=>{
     m.route(document.body,'/',{
         '/':{
+            
             view:()=>{
+                window.scrollTo(0,0);
                return m(Layout, m(Introduction))
             }
         },
         '/ranking1':{
             view:()=>{
+                window.scrollTo(0,0);
                return m(Layout, m(Ranking))
             }
         },
         '/ranking2':{
             view:()=>{
+                window.scrollTo(0,0);
                return m(Layout, m(RankingTwo))
             }
         },
         '/mc1':{
             view:()=>{
+                window.scrollTo(0,0);
                 return m(Layout, m(MC))
             }
         },
         '/partnership':{
             view:()=>{
+                window.scrollTo(0,0);
                 return m(Layout, m(PartnershipsView))
             }
         },
         '/pilots':{
             view:(vnode)=>{
-                
+                window.scrollTo(0,0);
                 return m(Layout, m(PilotsView, {partner:m.route.param("partner")}))
             }
         },
         '/discussion':{
             view:()=>{
+                window.scrollTo(0,0);
                return m(Layout, m(Discussion))
             }
         },
